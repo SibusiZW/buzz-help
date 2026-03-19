025dc84eb2542c1f143d1b7e5e7e0eb0c0e26275
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-19 16:49:08 +0200

    added gemini.ts server action

---
+++ server/gemini.ts
@@ -0,0 +1,18 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY! })
+
+
+export default async function generateResponse(prompt: string) {
+    if (prompt.trim() === "") return "Please enter prompt!";
+
+    const response = await ai.models.generateContent({
+        model: 'gemini-3-flash-preview',
+        contents: `Please generate a study plan for me using this info: \n${prompt}\n`,
+        config: {
+            systemInstruction: "You are a professional study plan drafter. You should be given the previous test scores, weak subjects and time left for exams for a student. Then draft the student a study plan"
+        }
+    })
+
+    return response.text
+}
