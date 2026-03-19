6a7f25ff7fc8d97ab40328afd79c49afcf55b302
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-19 17:22:06 +0200

    added markdown rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,46 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { BookOpenText, CodeSquare, Glasses } from "lucide-react";
+import generateResponse from "@/server/gemini";
+import { BookOpenText, CodeSquare, Glasses, Loader2 } from "lucide-react";
+import React, { useState } from "react";
+import ReactMarkdown from 'react-markdown';
 
 export default function Home() {
+
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e: React.SubmitEvent) {
+    e.preventDefault();
+
+    setLoading(true);
+    const res = await generateResponse(prompt);
+
+    setLoading(false);
+
+    if (res)  {
+      setResponse(res);
+    } else setResponse("Check API Key");
+    
+  }
+
+  function renderContent() {
+    if (loading) {
+      return <Loader2 className="animate-spin" />
+
+    } else {
+      return (
+        <>
+          <Glasses />
+          Get Help
+        </>
+      )
+    }
+  }
+
   return (
     <div className="min-h-screen p-2 flex flex-col items-center">
       
@@ -12,11 +50,10 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col w-full mb-4">
-        <form>
-          <Textarea placeholder="Enter your previous, test scores, weak subjects, Time left for exams here" className="mb-2"/>
+        <form onSubmit={handleSubmit}>
+          <Textarea value={prompt} placeholder="Enter your previous, test scores, weak subjects, Time left for exams here" className="mb-2" onChange={(e) => setPrompt(e.target.value)}/>
           <Button className={'bg-green-500 w-full'} type="submit">
-            <Glasses />
-            Get Help
+            {renderContent()}
           </Button>
         </form>
       </div>
@@ -27,7 +64,11 @@ export default function Home() {
       </div>
 
       <div className="w-full p-2">
-        <div className="w-full h-[400px] p-6 bg-white border border-zinc-200 rounded-2xl shadow-sm overflow-y-auto relative"></div>
+        <div className="w-full h-[400px] p-6 bg-white border border-zinc-200 rounded-2xl shadow-sm overflow-y-auto relative">
+          <span>
+            <ReactMarkdown>{response}</ReactMarkdown>
+          </span>
+        </div>
       </div>
     </div>
   );
